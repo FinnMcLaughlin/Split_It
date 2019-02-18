05ea91efb842e73fa7f5d97fae2d5f8cb9ea829e
FinnMcLaughlin <C15536837@mydit.ie>
2019-02-18 18:18:27 +0000

    Almost finished backing up

---
+++ index.js
@@ -0,0 +1,15 @@
+/** @format */
+
+import {AppRegistry} from 'react-native';
+import App from './Screens/App';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
+
+global.Symbol = require('core-js/es6/symbol');
+require('core-js/fn/symbol/iterator');
+require('core-js/fn/map');
+require('core-js/fn/set');
+require('core-js/fn/array/find');
+require("firebase/database");
+require("firebase/auth");
